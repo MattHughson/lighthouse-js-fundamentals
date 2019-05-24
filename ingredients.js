145a6c92df0d78c9b65c5f57fd41a86035cea689
MattHughson! <matt@koodomasson.com>
2019-05-24 23:09:17 +0000

    Adds looping exercises

---
+++ ingredients.js
@@ -0,0 +1,12 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var repeat = 0;
+while (repeat < ingredients.length){
+  console.log(ingredients[repeat]);
+  repeat++;
+}
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+for (var i = ingredients.length; i > 0; i--) {
+  console.log(ingredients[i]);
+}
